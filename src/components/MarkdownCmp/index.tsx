7e1e7c4caf6f27511e6f57438909b28aa7d54caf
shamir0xe <shamir0xe@gmail.com>
2025-01-02 05:32:34 +0330

    feat: Add markdown component renderer

---
+++ src/components/MarkdownCmp/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "./markdown.css";
+import "highlight.js/styles/github.css";
+
+type MarkdownCmpProps = {
+  content: string;
+};
+
+const MarkdownCmp = ({ content }: MarkdownCmpProps): JSX.Element => {
+  const [value, setValue] = useState<string>(content);
+
+  useEffect(() => {
+    if (content.length && content[0] == '"') {
+      content = content.slice(1, -1);
+    }
+    let regex: RegExp = /\\n/gi;
+    content = content.replace(regex, "\n");
+    console.log(content);
+    setValue(content);
+  }, [content]);
+
+  return (
+    <div className="markdown-container p-4">
+      <Markdown rehypePlugins={[rehypeHighlight]}>{value}</Markdown>
+    </div>
+  );
+};
+
+export default MarkdownCmp;
